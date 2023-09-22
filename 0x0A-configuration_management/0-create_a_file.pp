4390c615697c35630a554c21c63c717f20da42ba
SALAH-NAME <phone4ouahidi@gmail.com>
2023-09-22 22:06:40 +0000

    test

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# 0-create_a_file.pp
+file { 'school':
+  ensure  => 'present',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,5 @@
+# 0-create_a_file.pp
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
