15af5b7cf8b8593c1d42afe8695f78afd47e7eed
Your Name <you@example.com>
2024-01-29 22:47:29 +0100

    0x0f

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,8 @@
+# 2-puppet_custom_http_response_header.pp
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $hostname;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}
