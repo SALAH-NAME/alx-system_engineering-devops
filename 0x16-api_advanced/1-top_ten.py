7c148ca0aae7ad00ad63da8b8ee83306d640d12d
Your Name <you@example.com>
2024-03-05 22:40:35 +0100

    dfa

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' 1-top_ten.py '''
+
+
+import requests
+
+
+def top_ten(subreddit):
+    '''
+         1. Top Ten
+    '''
+    headers = {'User-Agent': 'custom'}
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)
+        return
+    posts = response.json()['data']['children']
+    for post in posts:
+        print(post['data']['title'])
