63db95145e65ba3440ee8ca6df6113606b252fa2
Your Name <you@example.com>
2024-06-18 15:52:23 +0000

    fix_0.1v

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,9 +9,13 @@ def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    url = 'http://www.reddit.com/r/{}/about.json'
-    user_agent = '0x16-api_advanced:project:v0.1.0 (by someone_unknown)'
-    r = requests.get(url.format(subreddit),
-                     headers={'User-Agent': user_agent}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.1.0 (by /u/vagrant)"
+    }
+    r = requests.get(url, headers=headers, allow_redirects=False)
+    if r.status_code == 404:
+        return 0
+    res = r.json().get("data")
+    subs = res.get("subscribers")
     return subs
