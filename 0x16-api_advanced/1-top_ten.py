d5171901f8507acd609fbf851ff01efa833f68f4
Your Name <you@example.com>
2024-06-18 16:27:04 +0000

    fix_1.2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,8 +12,10 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'custom'}
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
         posts = response.json()['data']['children']
+
         for post in posts:
             print(post['data']['title'])
     else:
