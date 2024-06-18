f9c3eb687ce7f1dddc4ed92b375c86764c925523
Your Name <you@example.com>
2024-06-18 16:17:32 +0000

    fix_1.0

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,7 +10,7 @@ def top_ten(subreddit):
          1. Top Ten
     '''
     headers = {'User-Agent': 'custom'}
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=9'.format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
